0e3661cee8244c2dbbf7b693376c498651befc79
Alejandro Vargas <alejandro.vargasmadrid@gmail.com>
2020-12-31 20:08:35 +0100

    fixed the play again buton

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const finalMessageRevealWord = document.getElementById('final-message-reveal-wor
 
 const figureParts = document.querySelectorAll('.figure-part');
 
-const words = ['sofa', 'isla', 'sillas', 'platos', 'cubiertos', 'armario', 'nutribullet', 'kitchenaid'];
+const words = ['hallacas', 'pan de jamon', 'ensalda', 'pernil', 'champan', 'cerveza', 'ponche crema'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
@@ -36,7 +36,28 @@ function displayWord() {
 
 // update the wrong letters
 function updateWrongLettersElement() {
-  console.log('Update wrong');
+  // display wrong letters
+  wrongLettersElement.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+  // display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if(index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  // check if lost
+  if(wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost 🙁🥺';
+    popup.style.display = 'flex';
+  }
 }
 
 // show notification
@@ -45,7 +66,7 @@ function showNotification() {
 
   setTimeout(() => {
     notification.classList.remove('show');
-  }, 2000)
+  }, 2000);
 }
 
 
@@ -73,6 +94,22 @@ window.addEventListener('keydown', e => {
       }
     }
   }
+});
+
+// restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  // empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersElement();
+
+  popup.style.display = 'none';
 });
 
 displayWord();
